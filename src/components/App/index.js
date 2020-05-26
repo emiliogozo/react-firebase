4fb04337fa023126b83eb9b14b026a56fca99103
emiliogozo <emilio.gozo@obf.ateneo.edu>
2020-05-27 02:42:05 +0800

    feat: SignUp component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 import Navigation from '../Navigation';
-
+import SignUpPage from '../SignUp';
 
 
 const App = () => {
@@ -11,8 +12,8 @@ const App = () => {
 
       <hr />
 
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
