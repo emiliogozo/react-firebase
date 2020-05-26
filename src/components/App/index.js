96fcfe40ba011ae66fd8ff5cb11c5f9ebe63d5e3
emiliogozo <emilio.gozo@obf.ateneo.edu>
2020-05-27 01:00:57 +0800

    chore: setup router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from '../Navigation';
+
+
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Navigation />
+
+      <hr />
+
+      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
+    </Router>
   )
 }
 
