13241492491a17c5b12ec5b8e943e7aa05c3d18a
emiliogozo <emilio.gozo@obf.ateneo.edu>
2020-05-27 11:26:37 +0800

    feat: PasswordChange and Account component

---
+++ src/components/Account/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PasswordChangeForm from '../PasswordChange';
+import { PasswordForgetForm } from '../PasswordForget';
+
+
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+);
+
+export default AccountPage;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import AccountPage from '../Account';
 import Navigation from '../Navigation';
 import PasswordForgetPage from '../PasswordForget';
 import { withAuthentication } from '../Session';
@@ -17,9 +18,10 @@ const App = () => {
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      
       <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
     </Router>
   );
