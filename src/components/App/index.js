ada74285695c361dd45600be0efd72d26a227cab
emiliogozo <emilio.gozo@obf.ateneo.edu>
2020-05-27 14:06:46 +0800

    feat: integrate firebase db

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import AccountPage from '../Account';
+import AdminPage from '../Admin';
 import HomePage from '../Home';
+import LandingPage from '../Landing';
 import Navigation from '../Navigation';
 import PasswordForgetPage from '../PasswordForget';
 import { withAuthentication } from '../Session';
@@ -16,13 +18,13 @@ const App = () => {
 
       <hr />
 
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
     </Router>
   );
 }
